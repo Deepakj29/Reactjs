32f238052e5cce7a79621895e1413cfd016a0ce9
Deepakj29 <103329335+Deepakj29@users.noreply.github.com>
2022-07-10 06:13:58 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Todo from "./Todolist..js/Todo";
+function App() {
+  return (
+    <>
+    <div className="App">
+    <Todo></Todo>
+    </div>
+    
+    
+    </>
+  );
+}
+
+export default App;
